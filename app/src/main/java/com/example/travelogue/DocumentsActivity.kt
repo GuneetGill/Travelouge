404d0ae17c69b104f8cbe3353a51e35d3b1445c6
Madina Vaninova <madi@Madinas-MacBook-Pro.local>
2024-11-05 17:05:59 -0800

    Fragment:settings

---
+++ app/src/main/java/com/example/travelogue/DocumentsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.travelogue
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelogue.R
+
+class DocumentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_documents)
+
+    }
+}
