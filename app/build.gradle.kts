ffccc21cdd3b20704174a54469053ce595a0854e
Guneet Gill <gkg26@sfu.ca>
2024-11-07 11:24:40 -0800

    completed expense fragment ui and added some code to fragment for testing pie chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -52,7 +51,9 @@ dependencies {
     implementation(libs.androidx.material3.android)
     implementation ("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation ("androidx.navigation:navigation-ui-ktx:2.6.0")
-
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("ir.mahozad.android:pie-chart:0.7.0")
+    implementation ("com.github.AnyChart:AnyChart-Android:1.1.5")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
