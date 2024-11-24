950954aead3c19906a016614569b6ac09365a8c8
Madina Vaninova <madi@Madinas-MacBook-Pro.local>
2024-11-24 01:51:52 -0800

    user can now edit their information

--- app/src/main/java/com/example/travelogue/db_user/UserDao.kt
+++ app/src/main/java/com/example/travelogue/db_user/UserDao.kt
@@ -15,4 +15,10 @@ interface UserDao {
     @Query("SELECT * FROM user_table")
     fun getAllUsers(): Flow<List<User>>
 
+    @Query("SELECT * FROM user_table WHERE userId = :id LIMIT 1")
+    suspend fun getUserById(id: Long): User?
+
+    @Query("UPDATE user_table SET user_first_name_col = :firstName, user_last_name_col = :lastName, user_name = :username WHERE userId = :userId")
+    suspend fun updateUserDetails(userId: Long, firstName: String, lastName: String, username: String)
+
 }
