0361fa7ff9f5b412d297de063ee03afdb7ebf910
Joshua Lawrence <72839167+Josh0187@users.noreply.github.com>
2024-11-27 21:03:57 -0800

    initial setup for the biometric login

--- app/src/main/java/com/example/travelogue/Util.kt
+++ app/src/main/java/com/example/travelogue/Util.kt
@@ -1,9 +1,12 @@
 package com.example.travelogue
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Build
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.LatLng
@@ -39,5 +42,47 @@ object Util {
         if(missingPermission){
             ActivityCompat.requestPermissions( activity!!, permissions, 0)
         }
+    }
+
+    // for biometric login
+    fun saveToken(context: Context, userID: Long) {
+        val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("user_token", userID.toString()).apply()
+        sharedPreferences.edit().putBoolean("fingerprint", true).apply()
+    }
+
+    fun isFingerprintEnabled(context: Context): Boolean? {
+        val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        if (sharedPreferences.contains("fingerprint")) {
+            return sharedPreferences.getBoolean("fingerprint", false)
+        }
+        return null
+    }
+
+    fun getToken(context: Context): Long? {
+        val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("user_token", null)?.toLong()
+    }
+
+    // Function to show the dialog
+    fun showEnableFingerprintDialog(context: Context, userID: Long) {
+        val intent = Intent(context, MainActivity::class.java)
+        val dialog = AlertDialog.Builder(context)
+            .setTitle("Enable Fingerprint Login")
+            .setMessage("Do you want to use fingerprint authentication for future logins?")
+            .setPositiveButton("Yes") { _, _ ->
+                // User agrees to enable fingerprint login
+                saveToken(context, userID)
+                Toast.makeText(context, "Fingerprint Authentication Enabled", Toast.LENGTH_SHORT).show()
+                context.startActivity(intent)
+            }
+            .setNegativeButton("No") { _, _ ->
+                // User declines
+                //saveFingerprintPreference(userID)
+                context.startActivity(intent)
+            }
+            .create()
+
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/travelogue/ui/login_reg/LoginActivity.kt
+++ app/src/main/java/com/example/travelogue/ui/login_reg/LoginActivity.kt
@@ -1,25 +1,35 @@
 package com.example.travelogue.ui.login_reg
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.travelogue.Globals.PREF_NAME
 import com.example.travelogue.MainActivity
 import com.example.travelogue.R
+import com.example.travelogue.Util
 import com.example.travelogue.db_user.UserDao
 import com.example.travelogue.db_user.UserDatabase
 import com.example.travelogue.db_user.UserRepository
 import com.example.travelogue.db_user.UserViewModel
 import com.example.travelogue.db_user.UserViewModelFactory
+import java.util.concurrent.Executor
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,12 +37,19 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var password: EditText
     private lateinit var loginButton: Button
     private lateinit var regButton: Button
+    private lateinit var biometricLogin: Button
 
     private lateinit var database: UserDatabase
     private lateinit var databaseDao: UserDao
     private lateinit var repository: UserRepository
     private lateinit var viewModelFactory: UserViewModelFactory
     private lateinit var userViewModel: UserViewModel
+
+    // biometric login stuff
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private val REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +61,7 @@ class LoginActivity : AppCompatActivity() {
         password = findViewById(R.id.user_pw)
         loginButton = findViewById(R.id.login_button)
         regButton = findViewById(R.id.register_button)
+        biometricLogin = findViewById(R.id.biometricLogin)
 
         //db initialize
         database = UserDatabase.getInstance(this)
@@ -90,7 +108,13 @@ class LoginActivity : AppCompatActivity() {
                 editor.putLong("user_id", userId)
                 Log.d("myuserid", "User : $userId")
                 editor.commit()
-                startActivity(intent)
+                // ask if user wants to enable fingerprint auth
+                if (Util.isFingerprintEnabled(this) == null && Util.getToken(this) != userId) {
+                    Util.showEnableFingerprintDialog(this, userId)
+                }
+                else {
+                    startActivity(intent)
+                }
             } else {
                 Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show()
             }
@@ -102,5 +126,95 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Biometric login
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                    if (errorCode == BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED) {
+                        // Prompts the user to create credentials that your app accepts.
+                        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                            putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                                BIOMETRIC_STRONG)
+                        }
+                        startActivityForResult(enrollIntent, REQUEST_CODE)
+                    }
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                    // go to home
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        // disable and grey out biometric auth button if not enabled
+        if (Util.isFingerprintEnabled(this) == null) {
+            biometricLogin.isEnabled = false
+            biometricLogin.alpha = 0.5f
+        }
+
+        biometricLogin.setOnClickListener {
+            // check that username/email field is filled out
+            val usernameOrEmail = usernameEmail.text.toString()
+            if (usernameOrEmail.isEmpty()) {
+                usernameEmail.performClick()
+                Toast.makeText(applicationContext, "Enter your username or email",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else {
+                var userId = -1L
+                userViewModel.allUsersLiveData.observe(this) { users ->
+                    if (users.isNotEmpty()) {
+                        users.forEach { user ->
+                            if ((user.userEmail == usernameOrEmail || user.userName == usernameOrEmail)) {
+                                userId = user.userId
+                            }
+                        }
+                    }
+                }
+                if (userId == -1L) {
+                    Toast.makeText(applicationContext, "Invalid Username or Email.",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+                // check that correct user is entered and has fingerprint enabled
+                if (Util.getToken(this) == userId) {
+                    biometricPrompt.authenticate(promptInfo)
+                }
+                else {
+                    Toast.makeText(applicationContext, "This user does not have fingerprint enabled. Please login with password.",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        }
+
     }
+
 }
