562c2e2f9baa4dd2fb13c5331b0efb92d410379d
Joshua Lawrence <72839167+Josh0187@users.noreply.github.com>
2024-11-29 16:21:38 -0800

    finished setting up biometric auth for documents + cleaned it up a bit

--- app/src/main/java/com/example/travelogue/Util.kt
+++ app/src/main/java/com/example/travelogue/Util.kt
@@ -8,15 +8,22 @@ import android.content.res.Resources
 import android.graphics.Bitmap
 import android.location.Geocoder
 import android.os.Build
+import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricPrompt
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.example.travelogue.Globals.PREF_NAME
+import com.example.travelogue.ui.doc.DocumentsActivity
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import java.util.Locale
+import java.util.concurrent.Executor
 
 object Util {
     // Geocoding stuff
@@ -83,6 +90,54 @@ object Util {
             .create()
 
         dialog.show()
+    }
+
+    fun getBiometricPrompt(context: Context, activity: FragmentActivity, successIntent: Intent): BiometricPrompt {
+
+        lateinit var executor: Executor
+        lateinit var biometricPrompt: BiometricPrompt
+        val REQUEST_CODE = 1
+
+        executor = ContextCompat.getMainExecutor(context)
+        biometricPrompt = BiometricPrompt(activity, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(context,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                    if (errorCode == BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED) {
+                        // Prompts the user to create credentials that your app accepts.
+                        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                            putExtra(
+                                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                                BIOMETRIC_STRONG
+                            )
+                        }
+                        activity.startActivityForResult(enrollIntent, REQUEST_CODE)
+                    }
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(context,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT)
+                        .show()
+                    // go to home
+                    val intent = Intent(context, DocumentsActivity::class.java)
+                    context.startActivity(successIntent)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(context, "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+        return biometricPrompt
     }
 
     // get userId
